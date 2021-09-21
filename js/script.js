0016eac7998953bf14fc8648a7990d98f19c3228
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2021-09-21 10:59:51 +0300

    designed movie item template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-function handleSearch(){
-    var searchText = document.getElementById('search_input').value;
-    alert(`// TODO searching movies: ${searchText}`);
-    // TODO
+var moviesSection;
+
+function handleSearch() {
+  var searchText = document.getElementById("search_input").value;
+  alert(`// TODO searching movies: ${searchText}`);
+  // TODO
+}
+
+window.onload = function () {
+  moviesSection = document.getElementById("movies");
+};
+
+function ClearList() {
+  while (moviesSection.lastElementChild) {
+    moviesSection.removeChild(moviesSection.lastElementChild);
+  }
+}
+
+function LoadMovies(){
+    
 }
