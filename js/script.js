fe2f98a007108fa71d764c3c2e7a3a9da37ba3c5
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2021-09-22 09:58:27 +0300

    Declare RenderMovies function

--- js/script.js
+++ js/script.js
@@ -36,9 +36,49 @@ async function LoadMovies() {
       total_pages = json["total_pages"];
       results = json["results"];
     });
-  console.log(results);
   RenderMovies(results);
 }
 
 function RenderMovies(movies) {
+  for (let i = 0; i < movies.length; i++) {
+    let movie = movies[i];
+    let title = movie["title"];
+    let rate = movie["vote_average"];
+    let year = new Date(movie["release_date"]).getFullYear();
+    let image = `${image_url}${movie["poster_path"]}`;
+
+    const title_div = document.createElement("div");
+    const rate_div = document.createElement("div");
+    const details_div = document.createElement("div");
+    const year_div = document.createElement("div");
+    const img_div = document.createElement("div");
+    const movieItem_div = document.createElement("div");
+    const col_div = document.createElement("div");
+
+    title_div.classList.add('title');
+    title_div.innerHTML = title;
+
+    rate_div.classList.add('rate');
+    rate_div.innerHTML = rate;
+
+    year_div.classList.add('year');
+    year_div.innerHTML = year;
+
+    details_div.classList.add('details');
+    details_div.append(title_div);
+    details_div.append(rate_div);
+
+    img_div.classList.add('img');
+    img_div.style["background-image"] = `url(${image})`;
+
+
+    movieItem_div.classList.add('movie-item');
+    movieItem_div.append(img_div);
+    movieItem_div.append(details_div);
+    movieItem_div.append(year_div); 
+ 
+    col_div.classList.add('col');
+    col_div.append(movieItem_div);
+    moviesSection.append(col_div);
+  }
 }
