d96619151047acf72d40a060393992cdd21eed7a
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2021-09-22 09:12:56 +0300

    Declare LoadMovies function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var moviesSection;
-
+const api_key = "cc2fb20d03dc927d5576605d11dd1bed";
+const api_url = "https://api.themoviedb.org/3/discover/movie?api_key=";
+const image_url = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
 function handleSearch() {
   var searchText = document.getElementById("search_input").value;
   alert(`// TODO searching movies: ${searchText}`);
@@ -8,6 +10,7 @@ function handleSearch() {
 
 window.onload = function () {
   moviesSection = document.getElementById("movies");
+  LoadMovies();
 };
 
 function ClearList() {
@@ -16,6 +19,26 @@ function ClearList() {
   }
 }
 
-function LoadMovies(){
-    
+async function LoadMovies() {
+  let url = `${api_url}${api_key}`;
+
+  let page, total_pages, results;
+
+  await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      page = json["page"];
+      total_pages = json["total_pages"];
+      results = json["results"];
+    });
+  console.log(results);
+  RenderMovies(results);
+}
+
+function RenderMovies(movies) {
 }
