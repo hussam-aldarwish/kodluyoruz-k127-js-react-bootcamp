cd34aa562b68af626837ef0af8c2bd99a3b02035
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2021-09-23 11:26:55 +0300

    Added Comments

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ const api_url = `${base_url}/discover/movie?${api_key}`;
 const search_url = `${base_url}/search/movie?${api_key}`;
 const image_base_url = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
 
+// declare variable to store last fetched url, to allow pagination of search results
 let last_url;
 
+// declare variable to store required html elements
 let moviesSection, pagination;
 
+// Declare function to load movies on page load
 window.onload = function () {
   moviesSection = document.getElementById("movies");
   pagination = document.getElementById("pagination");
@@ -15,6 +18,7 @@ window.onload = function () {
   last_url = api_url;
 };
 
+// Decalre function to fetch movies data from api endpoint
 async function LoadMovies(url) {
   await fetch(url, {
     method: "GET",
@@ -29,6 +33,7 @@ async function LoadMovies(url) {
     });
 }
 
+// Declare function to show movies on the movies section
 function RenderMovies(movies) {
   moviesSection.innerHTML = "";
   movies.forEach((movie) => {
@@ -55,6 +60,7 @@ function RenderMovies(movies) {
   });
 }
 
+// Declare function to determine rate class name upon vote_average
 function GetRateClass(rate) {
   switch (true) {
     case rate >= 7:
@@ -69,18 +75,23 @@ function GetRateClass(rate) {
   }
 }
 
+// Declare function to initialize pagination section when movies data is recieved
 function RenderPagination(page, total_pages) {
   pagination.innerHTML = "";
 
+  // First Page
   const firstPage_el = document.createElement("li");
   firstPage_el.innerHTML = '<a href="#" onclick="GotoPage(1)">«</a>';
 
+  // First Page
   const lastPage_el = document.createElement("li");
   lastPage_el.innerHTML = `<a href="#" onclick="GotoPage(${total_pages})">»</a>`;
 
+  // Current Page
   let currentPage_el = document.createElement("li");
   currentPage_el.innerHTML = `<a href="#" class="active" onclick="GotoPage(${page})">${page}</a>`;
 
+  // Determine number of previous pages upon current page, max = 3 preivous pages
   let prevPages = [];
   for (let i = page - 1; i > 0; i--) {
     if (prevPages.length < 3) {
@@ -91,6 +102,7 @@ function RenderPagination(page, total_pages) {
   }
   prevPages.reverse();
 
+  // Determine number of next pages upon current page, max = 3 next pages
   let nextPages = [];
   for (let i = page + 1; i <= total_pages; i++) {
     if (nextPages.length < 3) {
@@ -100,6 +112,7 @@ function RenderPagination(page, total_pages) {
     } else break;
   }
 
+  // Add page navigations to pagination element
   pagination.append(firstPage_el);
   prevPages.forEach((element) => {
     pagination.append(element);
@@ -111,11 +124,14 @@ function RenderPagination(page, total_pages) {
   pagination.append(lastPage_el);
 }
 
+
+// Declare a function to change page number
 function GotoPage(pageNumber) {
   let url = `${last_url}&page=${pageNumber}`;
   LoadMovies(url);
 }
 
+// Declare a function to search movies by title
 function handleSearch() {
   let searchText = document.getElementById("search_input").value;
   let url =
@@ -124,6 +140,7 @@ function handleSearch() {
   last_url = url;
 }
 
+// Declare a function to sort current movies
 function Sort() {
   let category = document.getElementById("sort-category").value;
   if (category == "") {
@@ -135,6 +152,8 @@ function Sort() {
     last_url = url;
   }
 }
+
+// Declare a function to unsort and reload movies
 function CancelSorting() {
   let category = document.getElementById("sort-category");
   category.value = "";
