f61fa23960c09caa94b08b52bb252bbbba6e5377
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2021-10-04 08:43:47 +0300

    define themeContext

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import { users as mock } from "./mocks/mock-users.json";
 import { v4 as uuid } from "uuid";
-import { userContext } from "./contexts/user";
+import { UserContext } from "./contexts/user";
+import { ThemeContext } from "./contexts/theme";
 
 function App() {
   // user context related states
@@ -41,6 +42,13 @@ function App() {
     });
   }
 
+  // theme related states
+  const [theme, setTheme] = useState();
+
+  function toggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
   // cascade states to local storage
   useEffect(() => {
     if (user) {
@@ -73,8 +81,13 @@ function App() {
     }
   }, [users]);
 
+  useEffect(() => {
+    if (theme) localStorage.setItem("theme", theme);
+  }, [theme]);
+
   // On load
   useEffect(() => {
+    // load user context related properties
     let user = localStorage.getItem("user");
     if (user) {
       user = JSON.parse(user);
@@ -90,12 +103,20 @@ function App() {
       users = JSON.parse(users);
       setUser(users);
     }
+    // load theme context related properties
+    const theme = localStorage.getItem("theme");
+    if (theme) setTheme(theme);
+    else setTheme("light");
   }, []);
 
   return (
-    <userContext.Provider
-      value={(user, selectedUser, user, login, logout, selectUser, sendMessage)}
-    ></userContext.Provider>
+    <ThemeContext value={(theme, toggleTheme)}>
+      <UserContext.Provider
+        value={
+          (user, selectedUser, user, login, logout, selectUser, sendMessage)
+        }
+      ></UserContext.Provider>
+    </ThemeContext>
   );
 }
 
