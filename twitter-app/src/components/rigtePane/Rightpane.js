60103bab1b7d856b94bf1fc9e7858e178ef92e67
abdulahhamad <alhmada176@gmail.com>
2021-10-19 16:08:56 +0300

    scss veribal

--- twitter-app/src/components/rigtePane/Rightpane.js
+++ twitter-app/src/components/rigtePane/Rightpane.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './RightPane.scss'
 
 export default function Rightpane() {
     return (
         <>
          <div>
-             
+
          </div>   
         </>
     )
